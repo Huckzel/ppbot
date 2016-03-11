f61dba7391a351f5c529f1d1578efd82ad07398b
Huck Nuchelmans <huck.nuchelmans@gmail.com>
2016-03-11 21:54:50 +0100

    Create new inline handler that also supports math mode.

--- ppbot.py
+++ ppbot.py
@@ -8,8 +8,11 @@ A simple Telegram bot that gives users the ability to use markdown.
 """
 
 import os
+import urllib
 import requests
-from telegram import Updater, InlineQueryResultArticle
+import cStringIO
+from PIL import Image
+from telegram import Updater, InlineQueryResultArticle, InlineQueryResultPhoto
 
 UPDATER = Updater(token=os.environ['TOKEN'])
 
@@ -26,7 +29,9 @@ def markdown(bot, update):
 def math(bot, update, args):
     """ Convert a text to a LaTeX Math image. """
     expression = ' '.join(args)
-    math_url = r'http://latex.codecogs.com/png.latex?\dpi{300}%s' % expression
+    math_url = math_url = \
+        r'https://latex.codecogs.com/png.latex?%5Cdpi%7B600%7D' + \
+        urllib.quote(expression)
 
     file_name = 'img/%s.png' % hash(expression)
     tmp_file = open(file_name, 'wb')
@@ -57,13 +62,44 @@ def inline_markdown(bot, update):
         bot.answerInlineQuery(update.inline_query.id, results)
 
 
+def inline_pretty(bot, update):
+    """ Pretty print a query. This can be markdown or LaTeX math. """
+    if update.inline_query is not None:
+        query = update.inline_query.query
+
+        if query[0] == '$' and query[-1] == '$':
+            # If the query is between dollar signs, interpret it as math.
+            math_url = \
+                r'https://latex.codecogs.com/png.latex?%5Cdpi%7B600%7D' + \
+                urllib.quote(query[1:-1])
+
+            # Get the dimensions of the image.
+            tmp_file = cStringIO.StringIO(urllib.urlopen(math_url).read())
+            width, height = Image.open(tmp_file).size
+
+            results = [
+                InlineQueryResultPhoto(
+                    'MATH', math_url, math_url, mime_type='image/png',
+                    photo_width=width, photo_height=height
+                )
+            ]
+        else:
+            results = [
+                InlineQueryResultArticle(
+                    'MD', 'Markdown', query, parse_mode='Markdown'
+                ),
+            ]
+
+        bot.answerInlineQuery(update.inline_query.id, results)
+
+
 def start():
     """ Handle telegram updates. """
     dispatcher = UPDATER.dispatcher
 
     dispatcher.addTelegramCommandHandler('math', math)
     dispatcher.addTelegramMessageHandler(markdown)
-    dispatcher.addTelegramInlineHandler(inline_markdown)
+    dispatcher.addTelegramInlineHandler(inline_pretty)
 
     UPDATER.start_polling()
 
